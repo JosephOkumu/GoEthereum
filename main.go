84552474d5d0ac0fdc17e330b7c5b4819c12c9c5
JosephOkumu <jokumu25@gmail.com>
2024-07-22 20:39:06 +0300

    Check current and pending balance

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
@@ -14,10 +15,27 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("we have a connection")
-	_ = client // we'll use this in the upcoming sections
+	_ = client
 
+	// Working with the address
 	address := common.HexToAddress("0xdAe6cC6fB09b0be1d6D6817D0E8Af85104a2Dfe7")
 	fmt.Println(address.Hex())
-	// fmt.Println(address.Hash().Hex()) 
-	fmt.Println(address.Bytes())     
+	// fmt.Println(address.Hash().Hex())
+	fmt.Println(address.Bytes())
+
+	// Checking balance
+	account := common.HexToAddress("0xdAe6cC6fB09b0be1d6D6817D0E8Af85104a2Dfe7")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("currrent balance :%v\n", balance)
+
+	// Checking the pending balance
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+
+	if err != nil {
+		fmt.Println("Error:%V", err)
+	}
+	fmt.Printf("pending balance: %v\n", pendingBalance)
 }
